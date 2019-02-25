762ce997911cf0463f973e8e32e768bbe5116e72
Daniel Sakol <45987919+DanielSakol@users.noreply.github.com>
2019-02-25 16:08:18 -0600

    Add files via upload

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,83 @@
+$( document ).ready(function() {
 
-var targetNumber = 53;
+const targetNumber = 33;
+let counter = 0;
+let winCounter= 0;
+let lossCounter= 0;
 
-$("#number-to-guess").text(targetNumber);
+// $("#number-to-guess").text(targetNumber);
 
-var counter = 0;
+//array of crystals numbers and crystals
+//const numberOptions = [5, 2, 10, 7];
+//const crystals = ["crystal-1", "crystal-2", "crystal-3", "crystal-4"];
 
-//array of crystals
-var numberOptions = [5, 2, 10, 7]
+//for loop 4 times?
+/* for (let i = 0; i < crystals.length; i++) {
+    const imageCrystal = $("#crystal-1");
 
-//for loop
-for (var i = 0; i < numberOptions.length; i++) {
-    var imageCrystal = $("<img>");
-    
-    imageCrystal.addClass("crystal-image");
     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+    
     $("#crystals").append(imageCrystal);
 }
+//crystal 2
+for (let i = 0; i < crystals.length; i++) {
+  const imageCrystal = $("#crystal-2");
+      
+  imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+  
+  $("#crystals").append(imageCrystal);
+}
+//crystal 3
+for (let i = 0; i < crystals.length; i++) {
+  const imageCrystal = $("#crystal-3");
+      
+  imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+  
+  $("#crystals").append(imageCrystal);
+}
+//crystal 4
+for (let i = 0; i < crystals.length; i++) {
+  const imageCrystal = $("#crystal-4");
+      
+  imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+  
+  $("#crystals").append(imageCrystal);
+}
+ */
 
+
+//onclick
 $(".crystal-image").on("click", function() {
-
-    var crystalValue = ($(this).attr("data-crystalvalue"));
+console.log("got here");
+//this statment
+    let crystalValue = $(this).attr("data-crystalvalue");
     crystalValue = parseInt(crystalValue);
-
+//counter (every click adds to counter)
     counter += crystalValue;
+    $("#current-score").text(counter).append(document.createTextNode( " = SCORE... Click Again!" ) );;
 
-    alert("New score: " + counter);
+//win if statement
+//alert("New score: " + counter);
 
-    if (counter === targetNumber) {
-        alert("You win!");
-      }
-  
-      else if (counter >= targetNumber) {
-        alert("You lose!!");
-      }
-  
-    });
+if (counter === targetNumber) {
+  winCounter ++;
+  alert("Nailed it! 33! You win!");
+  $("#wins").text(winCounter);
+  counter = 0;
+  $("#current-score").text(counter).append(document.createTextNode( " ...Let's Try Again!" ) );
+  $( "#wins" ).append(document.createTextNode( " WINS" ) );
+}
+
+else if (counter >= targetNumber) {
+  lossCounter ++;  
+  alert("You lose!!");
+  $("#losses").text(lossCounter);
+  counter = 0;
+  $("#current-score").text(counter).append(document.createTextNode( " ...Let's Try Again!" ) );
+  $( "#losses" ).append(document.createTextNode( " LOSSES" ) );
+
+  }
+
+});
+
+});
